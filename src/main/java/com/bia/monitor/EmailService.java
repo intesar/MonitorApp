8a60cdb99b6ea9cc9490031c8ab882e2c68c6494
Intesar Mohammed <mdshannan@gmail.com>
2012-05-04 00:23:02 -0700

    email capability

--- src/main/java/com/bia/monitor/EmailService.java
+++ src/main/java/com/bia/monitor/EmailService.java
@@ -1,15 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bia.monitor;
 
 /**
  *
  * @author intesar
  */
-public class EmailService {
-    public void send(String email, String subject, String body) {
-        
-    }
+public interface EmailService {
+
+    String SMTP_HOST_NAME = "smtp.gmail.com";
+    String SMTP_PORT = "465";
+    String EMAIL_FROM_ADDRESS = "team@zytoon.me";
+    String SEND_FROM_USERNAME = "team@zytoon.me";
+    String SEND_FROM_PASSWORD = "";
+    String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+    String EMAIL_CONTENT_TYPE = "text/html";
+    String EMAIL_SIGNATURE = "<br/>"
+            + "Thanks, <br/>"
+            + "Zytoon.me Team";
+
+    /**
+     * 
+     * @param toAddress
+     * @param car
+     * @param comment
+     */
+    void sendEmail(String toAddress, String subject, String body);
+    
 }
---
+++ src/test/java/com/bia/monitor/EmailServiceImplTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bia.monitor;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author intesar
+ */
+public class EmailServiceImplTest {
+
+    public EmailServiceImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sendEmail method, of class EmailServiceImpl.
+     */
+    @Test
+    public void testSendEmail() {
+        System.out.println("sendEmail");
+        String toAddress = "intesar@ymail.com";
+        String subject = "Monitor App Test!";
+        String body = "Test!";
+        EmailService instance = new EmailServiceImpl();
+        try {
+            instance.sendEmail(toAddress, subject, body);
+        } catch (RuntimeException ex) {
+            fail("The test case is a prototype.");
+        }
+        // TODO review the generated test code and remove the default call to fail.
+        //
+    }
+}
